d932be30ab03e396f93d45fbc8f0311d8d5f2490
d9q2r74r6h-cyber <d9q2r74r6h@privaterelay.appleid.com>
2026-05-27 16:42:41 -0400

    Optimizar layout SEO y agregar Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,59 +34,56 @@ export const metadata = {
     },
   };
 
-export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="es-CL">
-    <script
-  type="application/ld+json"
-  dangerouslySetInnerHTML={{
-    __html: JSON.stringify({
-      '@context': 'https://schema.org',
-      '@type': 'Bakery',
-      name: 'Panadería Maruxa',
-      url: 'https://panaderiamaruxa.cl',
-      image: 'https://panaderiamaruxa.cl/og-maruxa.png',
-      description:
-        'Panadería artesanal con panes, pastelería y tortas con retiro en local.',
-      address: {
-        '@type': 'PostalAddress',
-        addressCountry: 'CL',
-      },
-    }),
-  }}
-/>
-    
-    <body>{children}
-<Toaster
-  richColors
-  position="top-center"
-/>
-<Script id="meta-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;
-    n.push=n;
-    n.loaded=!0;
-    n.version='2.0';
-    n.queue=[];
-    t=b.createElement(e);
-    t.async=!0;
-    t.src=v;
-    s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}
-    (window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es-CL">
+      <body>
+        {children}
 
-    fbq('init', '322565593442022');
-    fbq('track', 'PageView');
-  `}
-</Script>
+        <Toaster richColors position="top-center" />
 
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Bakery',
+              name: 'Panadería Maruxa',
+              url: 'https://panaderiamaruxa.cl',
+              image: 'https://panaderiamaruxa.cl/og-maruxa.png',
+              description:
+                'Panadería artesanal con panes, pastelería y tortas con retiro en local.',
+              address: {
+                '@type': 'PostalAddress',
+                addressCountry: 'CL',
+              },
+            }),
+          }}
+        />
 
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
 
-</body>
-
-
-</html>}
-
-
+            fbq('init', '322565593442022');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </body>
+    </html>
+  );
+}
