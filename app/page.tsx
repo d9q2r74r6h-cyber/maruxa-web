e929163afb31ec8e7a129c6eac82b50f4a548e40
d9q2r74r6h-cyber <d9q2r74r6h@privaterelay.appleid.com>
2026-07-19 11:57:58 -0400

    Corrige retorno de recuperación de contraseña

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/Header';
 import { Home } from '@/components/Home';
+import { RecoveryRedirect } from '@/components/RecoveryRedirect';
 
 export default function Page() {
   return (
     <>
+      <RecoveryRedirect />
       <Header />
       <Home />
     </>
